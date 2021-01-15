87426888b5268854982369e8d915ea293626ba59
Zahnen Garner <zahnen@gmail.com>
2021-01-15 10:04:52 -0800

    add Directory route + database context to HomeController

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresTreatsContext _db;
+
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      return View();
+    }
+
+    [HttpGet("/directory/")]
+    public ActionResult Directory()
+    {
+      ViewBag.Treats = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
       return View();
     }
   }
